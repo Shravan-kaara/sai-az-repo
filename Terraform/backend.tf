252aa2c74cdadb3f3e5ed579123bbb49afbca516
Shravan <saishravan.k@kaaratech.com>
2026-07-27 21:21:51 +0530

    updated Terraform code

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    use_azuread_auth = true
+    resource_group_name  = "rg-sai-az-demo"
+    storage_account_name = "stgacttfstatedemo01"
+    container_name       = "tfstate01"
+    key                  = "sai-az-repo.tfstate"
+  }
 }
