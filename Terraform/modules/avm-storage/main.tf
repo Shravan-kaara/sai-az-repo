40de8a3bf505fdd7e9f39b34ae3eea13f7278f59
Shravan <saishravan.k@kaaratech.com>
2026-08-06 17:38:01 +0530

    Update storage account to use native azurerm resources

--- Terraform/modules/avm-storage/main.tf
+++ Terraform/modules/avm-storage/main.tf
@@ -2,19 +2,13 @@ data "azurerm_resource_group" "existing" {
   name = var.resource_group_name
 }
 
-module "avm_res_storage_storageaccount" {
-  source  = "Azure/avm-res-storage-storageaccount/azurerm"
-  version = "0.7.4"
-
-  name                            = var.storage_account_name
-  location                        = var.location
-  resource_group_name             = data.azurerm_resource_group.existing.name
-  account_tier                    = var.account_tier
-  account_replication_type        = var.account_replication_type
-  https_traffic_only_enabled      = true
-  public_network_access_enabled   = true
-  shared_access_key_enabled       = true
-  default_to_oauth_authentication = false
-
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = data.azurerm_resource_group.existing.name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  https_traffic_only_enabled = true
+  
   tags = var.tags
 }
--- Terraform/modules/avm-storage/outputs.tf
+++ Terraform/modules/avm-storage/outputs.tf
@@ -1,20 +1,20 @@
 output "storage_account_id" {
   description = "ID of the created Azure Storage Account."
-  value       = module.avm_res_storage_storageaccount.resource_id
+  value       = azurerm_storage_account.storage.id
 }
 
 output "storage_account_name" {
   description = "Name of the created Azure Storage Account."
-  value       = module.avm_res_storage_storageaccount.name
+  value       = azurerm_storage_account.storage.name
 }
 
 output "storage_account_primary_blob_endpoint" {
   description = "The primary blob endpoint of the storage account."
-  value       = module.avm_res_storage_storageaccount.storage_account.primary_blob_endpoint
+  value       = azurerm_storage_account.storage.primary_blob_endpoint
 }
 
 output "storage_account_primary_access_key" {
   description = "The primary access key for the storage account."
-  value       = module.avm_res_storage_storageaccount.storage_account.primary_access_key
+  value       = azurerm_storage_account.storage.primary_access_key
   sensitive   = true
 }
