b3defe5a440e4bb7c16c456f9fd216f092c4310b
Shravan <saishravan.k@kaaratech.com>
2026-08-06 17:50:30 +0530

    Fix Terraform formatting

--- Terraform/modules/avm-storage/main.tf
+++ Terraform/modules/avm-storage/main.tf
@@ -3,12 +3,12 @@ data "azurerm_resource_group" "existing" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = data.azurerm_resource_group.existing.name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
+  name                       = var.storage_account_name
+  resource_group_name        = data.azurerm_resource_group.existing.name
+  location                   = var.location
+  account_tier               = var.account_tier
+  account_replication_type   = var.account_replication_type
   https_traffic_only_enabled = true
-  
+
   tags = var.tags
 }
